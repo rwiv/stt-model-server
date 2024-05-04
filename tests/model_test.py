cfbf4d313ea2c715deb147f1f134f982d969fafd
rwiv <zc798546@gmail.com>
2024-05-04 15:19:53 +0900

    feat: apply CUDA

--- tests/model_test.py
+++ tests/model_test.py
@@ -2,8 +2,10 @@ from stt.model.model import transcribe
 
 
 def test_model():
-    model_name = "base"
+    # model_name = "base"
+    model_name = "small"
     # model_name = "medium"
     result = transcribe(model_name, "../assets/test1.mp3")
+    print()
     for elem in result:
-        print(elem)
+        print(elem["text"])
