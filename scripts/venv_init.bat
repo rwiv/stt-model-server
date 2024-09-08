ae77244abc77dfbb3066a0a8711e3ccc31c44791
rwiv <zc798546@gmail.com>
2024-09-08 11:57:12 +0900

    feat: update dependency versions

--- scripts/venv_init.bat
+++ scripts/venv_init.bat
@@ -1,5 +1,6 @@
 cd ..
 python -m venv .venv
-.venv\Scripts\pip.exe install torch==2.2.2 torchvision==0.17.2 torchaudio==2.2.2 --index-url https://download.pytorch.org/whl/cu121
+.venv\Scripts\pip.exe install torch==2.4.1 --index-url https://download.pytorch.org/whl/cu124
 .venv\Scripts\pip.exe install -r requirements.txt
+.venv\Scripts\pip.exe install -r requirements-dev.txt
 pause
